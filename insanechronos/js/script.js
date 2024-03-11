d7b6187ce40bfa8bfb08f308a550014bb0717239
InsaneChronos <121401167+InsaneChronos@users.noreply.github.com>
2024-03-11 22:03:51 +0500

    Update script.js

--- insanechronos/js/script.js
+++ insanechronos/js/script.js
@@ -30,34 +30,45 @@ if (window.innerHeight > window.innerWidth) {
 	const title = document.getElementsByClassName("project-title");
 	for (var i = 0; i < title.length; i++ )
 		{
-		title[i].style.width = "80%";
+		title[i].style.width = "calc(80% - 80px)";
 		title[i].style.fontSize = "14px";
+		title[i].style.borderRadius = "10px";
 		}
 
 	const card = document.getElementsByClassName("project-card");
 	for (var i = 0; i < card.length; i++ )
 		{
-		card[i].style.width = "calc(80% - 42px)";
+		card[i].style.width = "calc(80% - 40px)";
+		card[i].style.border = "0";
 		}
 
 	const logo = document.getElementsByClassName("project-logo");
 	for (var i = 0; i < logo.length; i++ )
 		{
-		logo[i].style.width = "100px";
-		logo[i].style.height = "100px";
+		logo[i].style.width = "250px";
+		logo[i].style.height = "250px";
+		logo[i].style.margin = "0 40px 20px 40px";
+		}
+
+	const info = document.getElementsByClassName("project-info");
+	for (var i = 0; i < info.length; i++ )
+		{
+		info[i].style.width = "250px";
 		}
 
 	const infoList = document.querySelectorAll(".project-info ul");
 	for (var i = 0; i < infoList.length; i++ )
 		{
+		infoList[i].style.width = "250px";
 		infoList[i].style.fontSize = "14px";
+		infoList[i].style.paddingInlineStart = "0";
 		}
 
 	const links = document.getElementsByClassName("project-links");
 	for (var i = 0; i < links.length; i++ )
 		{
 		links[i].style.marginTop = "0";
-		links[i].style.marginLeft = "40px";
+		links[i].style.marginLeft = "0";
 		links[i].style.gap = "10px";
 		}
 
